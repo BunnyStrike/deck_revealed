5866773ad1e5455c79e5b459252fdfe83cce75cd
James Brinkerhoff <th3brink@gmail.com>
2022-09-23 10:24:00 -0600

    improved scripts

--- boot_videos/futurama/install.sh
+++ boot_videos/futurama/install.sh
@@ -2,10 +2,10 @@
 
 cd "$HOME/Downloads"
 mkdir -p futurama_boot_video
-cd futurama_boot_video
+cd "$HOME/Downloads/futurama_boot_video"
 wget https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/futurama/deck_startup.webm
 wget https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/library.css
 curl -o - https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/install_boot_screen.sh | bash -
 curl -o - https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/fix_full_screen.sh | bash -
-cd /..
+cd "$HOME/Downloads"
 rm -r futurama_boot_video
