a663f2994ca052fca453c4119aab46b73d3bda18
James Brinkerhoff <th3brink@gmail.com>
2022-09-22 22:57:31 -0600

    init

---
+++ futurama_steam_deck_boot/fix_full_screen.sh
@@ -0,0 +1,5 @@
+#!binbash
+
+cp "$HOME/.local/share/Steam/steamui/css/library.css" "$HOME/.local/share/Steam/steamui/css/library.css.original"
+echo 'video{flex-grow:1;width:100%; height:100%; z-index:10}' >> "$HOME/.local/share/Steam/steamui/css/library.css"
+truncate -r "$HOME/.local/share/Steam/steamui/css/library.css.original" "$HOME/.local/share/Steam/steamui/css/library.css"
---
+++ futurama_steam_deck_boot/install_boot_screen.sh
@@ -0,0 +1,4 @@
+#!binbash
+
+cp "$HOME/.local/share/Steam/steamui/movies/deck_startup.webm" "$HOME/.local/share/Steam/steamui/movies/deck_startup.webm.original"
+cp ./deck_startup.webm "$HOME/.local/share/Steam/steamui/movies/deck_startup.webm"
