ef5d4498b515dd0fa08f8bbb93098bf95b971350
James Brinkerhoff <th3brink@gmail.com>
2022-09-23 20:49:13 -0600

    fix

--- boot_videos/custom/custom_install.sh
+++ boot_videos/custom/custom_install.sh
@@ -1,5 +1,7 @@
 #!binbash
 
-wget https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/library.css
+# wget https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/library.css
 curl -o - https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/install_boot_screen.sh | bash -
 curl -o - https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/fix_full_screen.sh | bash -
+# 1840847 might need to change after updates 
+truncate -s 1840847 "$HOME/.local/share/Steam/steamui/movies/deck_startup.webm"
