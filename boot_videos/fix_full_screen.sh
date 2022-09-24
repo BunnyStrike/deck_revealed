9efe0b471f1e365534d6c89ea9dd62495eaf718d
Raspen83 <raspen83@gmail.com>
2022-09-24 20:52:15 +0200

    Add size detection for .css
    There is a size difference between the revisions of the steam deck.

--- boot_videos/fix_full_screen.sh
+++ boot_videos/fix_full_screen.sh
@@ -4,5 +4,8 @@ cp "$HOME/.local/share/Steam/steamui/css/library.css" "$HOME/.local/share/Steam/
 # cp "./library.css" "$HOME/.local/share/Steam/steamui/css/library.css"
 # echo 'video{flex-grow:1;width:100%; height:100%; z-index:10}' >> "$HOME/.local/share/Steam/steamui/css/library.css"
 sed -i 's/video{flex-grow:0;width:300px;height:300px;z-index:10}/video{flex-grow:1;width:100%; height:100%; z-index:10}/g' "$HOME/.local/share/Steam/steamui/css/library.css"
-# truncate -s 38488 "$HOME/.local/share/Steam/steamui/css/library.css"
+# Add automatic size detection if file size has changed
+size="$(wc -c <"$HOME/.local/share/Steam/steamui/css/library.css")"
+# Still need to truncate, otherwise when steam starts up, it will be replaced
+truncate -s $size "$HOME/.local/share/Steam/steamui/css/library.css"
 # truncate -r "$HOME/.local/share/Steam/steamui/css/library.css.original" "$HOME/.local/share/Steam/steamui/css/library.css"
