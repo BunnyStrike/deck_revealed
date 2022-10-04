61bb5db48843eba27868181b6d68b1c4860afb6c
James Brinkerhoff <th3brink@gmail.com>
2022-10-04 00:06:02 -0600

    testing beta install

--- boot_videos/futurama/beta_install.sh
+++ boot_videos/futurama/beta_install.sh
@@ -1,9 +1,9 @@
 #!binbash
 
-movies="$HOME/.local/share/Steam/steamui/overrides/movies"
+# movies="$HOME/.local/share/Steam/steamui/overrides/movies"
 
-mkdir -p "$movies"
+mkdir -p "$HOME/.local/share/Steam/steamui/overrides/movies"
 
-cd "$movies"
+cd "$HOME/.local/share/Steam/steamui/overrides/movies"
 
 wget https://raw.githubusercontent.com/BunnyStrike/deck_revealed/main/boot_videos/futurama/deck_startup.webm
