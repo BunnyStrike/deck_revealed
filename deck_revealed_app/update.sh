9d2851e3bd907ba275ae959341cbda32a37bf412
James Brinkerhoff <th3brink@gmail.com>
2023-03-04 11:07:38 -0700

    Add command to kill app with path

--- deck_revealed_app/update.sh
+++ deck_revealed_app/update.sh
@@ -16,20 +16,12 @@ report_error() {
 trap report_error ERR
 
 # Kill DeckRevealed if it is running
-killall -9 -q deck_revealed_app
+killall -9 -q deck_revealed_app || :
+kill $(pgrep -f /home/deck/Applications/DeckRevealed.AppImage)
 
 # Installs Chrome and allows controller support
 flatpak install --system -y com.google.Chrome
 flatpak --user override --filesystem=/run/udev:ro com.google.Chrome
-
-
-# Make Directory for Applications
-mkdir -p ~/Applications
-
-# Download and install app image launcher so Steam can run appimages from shortucts
-curl -L "${APP_IMAGE_LAUNCHER_URL}" -o ~/Downloads/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\n#Download Speed\:\2/' | zenity --progress --title "Downloading App Image Launcher" --width 600 --auto-close --no-cancel 2>/dev/null
-chmod +x ~/Downloads/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
-~/Downloads/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage install || :
 
 # Download and install DeckRevealed
 curl -L "${DECK_REVEALED_URL}" -o ~/Applications/DeckRevealed.AppImage 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\n#Download Speed\:\2/' | zenity --progress --title "Downloading DeckRevealed App" --width 600 --auto-close --no-cancel 2>/dev/null
